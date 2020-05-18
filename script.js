c9b1736e0ada65e9cfc74ca960aa5d87d127aece
Tatsyana Veraksa <veraksa161@gmail.com>
2020-05-18 04:25:02 +0300

    hmmmm

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  */
 
  const ulEl = document.getElementById('ul');
- const bodyEl = document.getElementById('container');
+ const contEl = document.getElementById('container');
 
 
 ulEl.addEventListener('click', function (ev) {
@@ -13,7 +13,7 @@ ulEl.addEventListener('click', function (ev) {
    }
  });
 
- bodyEl.addEventListener('click', function () {
+ contEl.addEventListener('click', function () {
    let el = document.getElementsByClassName('boldText');
    for(let i = 0; i<el.length; i++) {
       el[i].classList.remove('boldText');
@@ -24,6 +24,20 @@ ulEl.addEventListener('click', function (ev) {
  * 2. Написать код, чтобы при нажатии на li 
  *  2.1 его текст становился жирным 
  *  2.2 его текст (!) выводился в alert 
-    
+
     при нажатии вне ul выделение слетает (на пустом месте страницы)
  */
+
+const exClass1 = document.getElementsByClassName('example_class_1')[0];
+const bodyEl = document.getElementsByTagName("body")[0];
+
+exClass1.addEventListener('click', function(ev) {
+   if(ev.target.nodeName === 'LI') {
+      alert(ev.target.innerHTML);
+      ev.target.classList.add('boldText');
+   }
+});
+
+
+
+
